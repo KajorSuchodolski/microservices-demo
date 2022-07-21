969d7f6be272a152bfea0505cbecbb369e5ec01e
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-07-21 21:56:16 +0200

    Implemented easy RestControllerAdvisor to map exceptions to responses

---
+++ src/main/java/com/example/microservicesdemo/advice/ControllerAdvisor.java
@@ -0,0 +1,29 @@
+package com.example.microservicesdemo.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Map<String, String > body = new HashMap<>();
+        body.put("timestamp", Instant.now().toString());
+        body.put("status", Integer.toString(status.value()));
+        body.put("error", status.getReasonPhrase());
+        body.put("message", e.getMessage());
+
+        return ResponseEntity.badRequest().body(body);
+    }
+}
