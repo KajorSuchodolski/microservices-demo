5e1512d4176a05a58b3a25ba2c7d9b2af7553e62
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-07-21 20:59:36 +0200

    Excluded SecurityAutoConfiguration class for testing

--- src/main/java/com/example/microservicesdemo/MicroservicesDemoApplication.java
+++ src/main/java/com/example/microservicesdemo/MicroservicesDemoApplication.java
@@ -2,12 +2,13 @@ package com.example.microservicesdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MicroservicesDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesDemoApplication.class, args);
+    }
 
 }
