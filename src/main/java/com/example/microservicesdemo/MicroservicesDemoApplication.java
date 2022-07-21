956da207f4c4d1b84e97c0af6ff2737c280870fb
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-07-21 21:07:04 +0200

    Added Account entity

--- src/main/java/com/example/microservicesdemo/MicroservicesDemoApplication.java
+++ src/main/java/com/example/microservicesdemo/MicroservicesDemoApplication.java
@@ -1,11 +1,18 @@
 package com.example.microservicesdemo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MicroservicesDemoApplication {
+
+    @Bean
+    public Faker faker() {
+        return new Faker();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesDemoApplication.class, args);
---
+++ src/main/java/com/example/microservicesdemo/entity/Account.java
@@ -0,0 +1,58 @@
+package com.example.microservicesdemo.entity;
+
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "account", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "login", name = "login_unique_constraint")
+}, indexes = {
+        @Index(columnList = "id", name = "id_index"),
+        @Index(columnList = "login", name = "login_index"),
+})
+@NoArgsConstructor
+@ToString
+public class Account {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @Getter
+    @Setter
+    private UUID id;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    @Basic(optional = false)
+    @Getter
+    @Setter
+    private Long version;
+
+    @Column(name = "login", nullable = false)
+    @Basic(optional = false)
+    @Getter
+    @Setter
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    @Basic(optional = false)
+    @Getter
+    @Setter
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    @Basic(optional = false)
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @Basic(optional = false)
+    @Getter
+    @Setter
+    private String lastName;
+}
